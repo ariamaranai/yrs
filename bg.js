a94591e439e7a500abc4271a4d8a1716a58e7b35
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-05-06 22:32:20 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -6,14 +6,23 @@ chrome.omnibox.onInputChanged.addListener(async (text, suggest) => {
       "&src=realtime&appid=dj0zaiZpPVU5MGlSOUZ4cHVLbCZzPWNvbnN1bWVyc2VjcmV0Jng9ZGQ-&query=" +
       encodeURIComponent(text)
     )).text();
-    (data = data.slice(data.indexOf(",", 31) + 3, -10).split('","')).pop();
-    suggest(data.map(v => ({ content: v, description: v })));
+    (data = data.slice(data.indexOf(",", 31) + 3, -10).split('","'));
+    let s = data.length - 1;
+    if (s) {
+      let results = Array(s);
+      let i = 0;
+      while (
+        s = data[i],
+        results[i] = { content: s, description: s },
+        ++i < results.length
+      );
+      suggest(results);
+    }
   }
 });
 chrome.omnibox.onInputEntered.addListener(text =>
   chrome.tabs.query({ active: !0, currentWindow: !0 }, tabs =>
-    text &&
-    chrome.tabs.update(tabs[0].id, {
+    text && chrome.tabs.update(tabs[0].id, {
       url: "https://search.yahoo.co.jp/realtime/search?p=" + text
     })
   )
