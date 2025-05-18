e7068408e00b992cdf26ee503ceb9bd8301087b4
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-05-19 06:19:26 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,45 @@
-// var k = {};
-Object.prototype.hasOwnProperty.call = (a, b) => {
-  switch (b) {
-    case "animationBegin":
-    case "animationDuration":
-    case "animationEasing":
-    case "animationId":
-    case "autoCapitalize":
-    case "autoComplete":
-    case "autoCorrect":
-    case "color":
-    case "data-test":
-    case "onAnimationEnd":
-    case "onAnimationStart":
-    case "onTouchEnd":
-    case "onTouchMove":
-    case "onTouchStart":
-    case "referer":
-    case "role":
-    case "tabIndex":
-    case "xmlns":
-      return 0;
-    default:
-      return b in a;
+// var z = {};
+{
+  let has = (a, b) => {
+    switch (b) {
+      case "alt":
+      case "altKey":
+      case "androidTablet":
+      case "animation":
+      case "animationBegin":
+      case "animationDuration":
+      case "animationEasing":
+      case "animationId":
+      case "autoCapitalize":
+      case "autoComplete":
+      case "autoCorrect":
+      case "color":
+      case "ctrlKey":
+      case "data-test":
+      case "onAnimationEnd":
+      case "onAnimationStart":
+      case "onTouchEnd":
+      case "onTouchMove":
+      case "onTouchStart":
+      case "radius":
+      case "referer":
+      case "role":
+      case "shape":
+      case "shiftKey":
+      case "tabIndex":
+      // case "xAxis":
+      // case "xAxisId":
+      case "xmlns":
+      // case "yAxis":
+      // case "yAxisId":
+      case "yads":
+      case "yadsItem":
+        return 0;
+      default:
+        // b && typeof b == "string" && b.length > 2 && (z[b] ??= 0, ++z[b]);
+        return b in a;
+    }
   }
-  // b && typeof b == "string" && b.length > 2 && (k[b] ??= 0, ++k[b]);
+  Object.prototype.hasOwnProperty = function (a) { return has(this, a) }
+  Object.prototype.hasOwnProperty.call = has;
 }
