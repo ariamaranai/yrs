91e948122ac8288780aa8fbf75a18bfb3be3e21b
a <106607480+ariamaranai@users.noreply.github.com>
2025-09-02 15:13:47 +0900

    Update bg.js

--- bg.js
+++ bg.js
@@ -1,6 +1,6 @@
 chrome.omnibox.onInputChanged.addListener(async (text, suggest) => {
   try {
-    let data = await (await fetch ("https://assist-search.yahooapis.jp/SuggestSearchService/V3/webassistSearch?callback=fetchSuggest_2147483647&src=realtime&appid=dj0zaiZpPVU5MGlSOUZ4cHVLbCZzPWNvbnN1bWVyc2VjcmV0Jng9ZGQ-&query=" + encodeURIComponent(text))).text();
+    let data = await (await fetch("https://assist-search.yahooapis.jp/SuggestSearchService/V3/webassistSearch?callback=fetchSuggest_2147483647&src=realtime&appid=dj0zaiZpPVU5MGlSOUZ4cHVLbCZzPWNvbnN1bWVyc2VjcmV0Jng9ZGQ-&query=" + encodeURIComponent(text))).text();
     (data = data.slice(data.indexOf(",", 31) + 3, -10).split('","')).pop();
     let len = data.length;
     if (len) {
