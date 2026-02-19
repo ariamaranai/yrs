36c06110ec96188ad1b8aeaf47e01a14c6d2910e
a <106607480+ariamaranai@users.noreply.github.com>
2026-02-19 21:52:52 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,6 +1,6 @@
 chrome.omnibox.onInputChanged.addListener(async (text, suggest) => {
   if (navigator.onLine) {
-    let data = await (await fetch ("https://assist-search.yahooapis.jp/SuggestSearchService/V3/webassistSearch?callback=fetchSuggest_2147483647&src=realtime&appid=dj0zaiZpPVU5MGlSOUZ4cHVLbCZzPWNvbnN1bWVyc2VjcmV0Jng9ZGQ-&query=" + encodeURIComponent(text))).text();
+    let data = await (await fetch("https://assist-search.yahooapis.jp/SuggestSearchService/V3/webassistSearch?callback=fetchSuggest_2147483647&src=realtime&appid=dj0zaiZpPVU5MGlSOUZ4cHVLbCZzPWNvbnN1bWVyc2VjcmV0Jng9ZGQ-&query=" + encodeURIComponent(text))).text();
     (data = data.slice(data.indexOf(",", 31) + 3, -10).split('","')).pop();
     let len = data.length;
     if (len) {
@@ -15,8 +15,6 @@ chrome.omnibox.onInputChanged.addListener(async (text, suggest) => {
 });
 chrome.omnibox.onInputEntered.addListener(text =>
   chrome.tabs.query({ active: !0, currentWindow: !0 }, tabs =>
-    text && chrome.tabs.update(tabs[0].id, {
-      url: "https://search.yahoo.co.jp/realtime/search?p=" + text
-    })
+    text && chrome.tabs.update(tabs[0].id, { url: "https://search.yahoo.co.jp/realtime/search?p=" + text })
   )
 );
