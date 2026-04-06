89be0b3321fab7a22e8c7781a6b130be3018273b
a <106607480+ariamaranai@users.noreply.github.com>
2026-04-06 11:19:13 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,3 +1,9 @@
+chrome.contextMenus.onClicked.addListener((info, tab) =>
+  chrome.tabs.create({
+    url:"https://search.yahoo.co.jp/realtime/search?p=" + info.selectionText,
+    index: tab.index + 1
+  })
+);
 chrome.omnibox.onInputChanged.addListener(async (text, suggest) => {
   try {
     let data = await (await fetch("https://assist-search.yahooapis.jp/SuggestSearchService/V3/webassistSearch?callback=fetchSuggest_2147483647&src=realtime&appid=dj0zaiZpPVU5MGlSOUZ4cHVLbCZzPWNvbnN1bWVyc2VjcmV0Jng9ZGQ-&query=" + encodeURIComponent(text))).text();
@@ -14,4 +20,11 @@ chrome.omnibox.onInputEntered.addListener(text =>
   chrome.tabs.query({ active: !0, currentWindow: !0 }, tabs =>
     text && chrome.tabs.update(tabs[0].id, { url: "https://search.yahoo.co.jp/realtime/search?p=" + text })
   )
+);
+chrome.runtime.onInstalled.addListener(() =>
+  chrome.contextMenus.create({
+    id: "",
+    title: 'Search for "%s"',
+    contexts: ["selection"]
+  })
 );
