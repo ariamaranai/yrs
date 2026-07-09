b90ce9f0d1278a2fbbc1c0bd942598b67de13f60
a <106607480+ariamaranai@users.noreply.github.com>
2026-07-10 07:10:44 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -6,7 +6,7 @@ chrome.contextMenus.onClicked.addListener((info, tab) =>
 );
 chrome.omnibox.onInputChanged.addListener(async (text, suggest, $0) => {
   try {
-    let data = await (await fetch("https://assist-search.yahooapis.jp/SuggestSearchService/V3/webassistSearch?results=9&callback=fetchSuggest_2147483647&src=realtime&appid=dj0zaiZpPVU5MGlSOUZ4cHVLbCZzPWNvbnN1bWVyc2VjcmV0Jng9ZGQ-&query=" + encodeURIComponent(text))).text();
+    let data = await (await fetch("https://assist-search.yahooapis.jp/SuggestSearchService/V3/webassistSearch?results=9&callback=fetchSuggest_2e9&src=realtime&appid=dj0zaiZpPVU5MGlSOUZ4cHVLbCZzPWNvbnN1bWVyc2VjcmV0Jng9ZGQ-&query=" + encodeURIComponent(text))).text();
     let i = (data = JSON.parse(data.slice(data.indexOf(",", 31) + 1, -8))).length;
     if (i) {
       while (
