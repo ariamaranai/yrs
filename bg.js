b8bc3c3a142b251588e33dda005db1ab0ef8fc45
a <106607480+ariamaranai@users.noreply.github.com>
2026-08-21 10:40:24 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -10,15 +10,14 @@ chrome.omnibox.onInputChanged.addListener((text, suggest, $0) => {
   fetch("https://assist-search.yahooapis.jp/SuggestSearchService/V3/webassistSearch?callback=fetchSuggest_2e9&src=realtime&appid=dj0zaiZpPVU5MGlSOUZ4cHVLbCZzPWNvbnN1bWVyc2VjcmV0Jng9ZGQ-&query=" + encodeURIComponent(text))
   .then(r => r.text())
   .then(r => {
-    let data = JSON.parse(r.slice(r.indexOf(",", 31) + 1, -8));
-    let i = data.length;
+    let i = (r = JSON.parse(r.slice(r.indexOf(",", 31) + 1, -8))).length;
     if (!i)
       return;
     while (
-      data[--i] = { content: $0 = data[i], description: $0 },
+      r[--i] = { content: $0 = r[i], description: $0 },
       i
     );
-    return suggest(data);
+    return suggest(r);
   });
 });
 chrome.omnibox.onInputEntered.addListener(text =>
