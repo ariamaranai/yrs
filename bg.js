47db01ef89a297fe0d06f66e84394393594bab80
a <106607480+ariamaranai@users.noreply.github.com>
2026-08-21 10:27:31 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,26 +1,32 @@
+onunhandledrejection = e => e.preventDefault();
+
 chrome.contextMenus.onClicked.addListener((info, tab) =>
   chrome.tabs.create({
     url:"https://search.yahoo.co.jp/realtime/search?p=" + info.selectionText,
     index: tab.index + 1
   })
 );
-chrome.omnibox.onInputChanged.addListener(async (text, suggest, $0) => {
-  try {
-    let data = await (await fetch("https://assist-search.yahooapis.jp/SuggestSearchService/V3/webassistSearch?results=9&callback=fetchSuggest_2e9&src=realtime&appid=dj0zaiZpPVU5MGlSOUZ4cHVLbCZzPWNvbnN1bWVyc2VjcmV0Jng9ZGQ-&query=" + encodeURIComponent(text))).text();
-    let i = (data = JSON.parse(data.slice(data.indexOf(",", 31) + 1, -8))).length;
-    if (i) {
-      while (
-        $0 = data[--i],
-        data[i] = { content: $0, description: $0 },
-        i
-      );
-    }
+chrome.omnibox.onInputChanged.addListener((text, suggest, $0) => {
+  fetch("https://assist-search.yahooapis.jp/SuggestSearchService/V3/webassistSearch?callback=fetchSuggest_2e9&src=realtime&appid=dj0zaiZpPVU5MGlSOUZ4cHVLbCZzPWNvbnN1bWVyc2VjcmV0Jng9ZGQ-&query=" + encodeURIComponent(text))
+  .then(r => r.text())
+  .then(r => {
+    let data = JSON.parse(r.slice(r.indexOf(",", 31) + 1, -8));
+    let i = data.length;
+    if (!i)
+      return;
+    while (
+      data[--i] = { content: $0 = data[i], description: $0 },
+      i
+    );
     return suggest(data);
-  } catch {}
+  });
 });
 chrome.omnibox.onInputEntered.addListener(text =>
   chrome.tabs.query({ active: !0, currentWindow: !0 }, tabs =>
-    text && chrome.tabs.update(tabs[0].id, { url: "https://search.yahoo.co.jp/realtime/search?p=" + encodeURIComponent(text) })
+    text && chrome.tabs.update(
+      tabs[0].id,
+      { url: "https://search.yahoo.co.jp/realtime/search?p=" + encodeURIComponent(text) }
+    )
   )
 );
 chrome.runtime.onInstalled.addListener(() =>
